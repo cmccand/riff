56bf004e008f3601fd7d002c9c853b3080db9103
Chris McCandlish <cmccand@gmail.com>
2015-03-12 00:54:38 -0400

    committing with audio showing up in record.html view

--- app/assets/javascripts/controllers/controller.js
+++ app/assets/javascripts/controllers/controller.js
@@ -3,9 +3,9 @@
   angular.module('RiffApp')
   .controller('RiffController', function (RiffService,$location,$routeParams) {
     var riffCtrl = this;
-
-    RiffService.getRiffs().success(function(data) {
-      RiffCtrl.riffs = data;
+    riffCtrl.riffs = RiffService.getRiffs();
+    // RiffService.getRiffs().success(function(data) {
+    //   RiffCtrl.riffs = data;
     });
 
   });
